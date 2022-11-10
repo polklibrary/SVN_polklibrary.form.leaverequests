c9fc70bd96292fdb57f57779622ce9b57666ddd1
hietpasd <davidhietpas@gmail.com>
2022-11-10 10:39:26 -0600

    fixed double email from outlook

--- polklibrary.form.leaverequests/src/polklibrary/form/leaverequests/browser/ws_approval.py
+++ polklibrary.form.leaverequests/src/polklibrary/form/leaverequests/browser/ws_approval.py
@@ -35,43 +35,31 @@ class WSView(BrowserView):
             )
             if brains:
                 obj = brains[0].getObject()
-                obj.workflow_status = status
-                obj.reindexObject()
-                
-                if obj.workflow_status == 'approved':
-                    AddEventMailed(token, obj.title, obj.supervisors, obj.timeoff)
-                    # ids = []
-                    # days = obj.timeoff.replace(u'\r',u'').split(u'\n')
-                    # for day in days:
-                        # data = day.split(u'|')
-                        # resp = AddEventToGCAL(obj.title, data[1], data[2], data[0], data[3], data[4])
-                        # if(resp['status']) == 200:
-                            # ids.append(resp['response']['id'])
-                        # else:
-                            # # retry again
-                            # time.sleep(1)
-                            # resp = AddEventToGCAL(obj.title, data[1], data[2], data[0], data[3], data[4])
-                            # if(resp['status']) == 200:
-                                # ids.append(resp['response']['id'])
-                    # obj.gcal_event_id = u'|'.join(ids)
-                # obj.reindexObject()
+                if obj.workflow_status not in ['approved', 'denied']:
+                    obj.workflow_status = status
+                    obj.reindexObject()
+                    body = """
+                        <div style="border: 1px solid #888; background-color: #eee; padding: 10px; margin-top: 10px; max-width: 500px;">
+                            <div style="font-weight: bold; margin-left: 10px;">Your leave request was <u>${status}</u> by ${approver}</div>
+                            <div style="margin: 10px;">${timeoff}</div>
+                            <div style="margin: 10px;">
+                                <a style="background-color:#2959af; color:white; display:inline-block; font-weight:bold; margin:10px; padding:5px 10px; text-decoration: none;" href="${link}">View Request</a>
+                            </div>
+                        </div> 
+                    """
+                    body = body.replace('${approver}', obj.supervisors)
+                    body = body.replace('${timeoff}', TimeOffFormater(obj.timeoff))
+                    body = body.replace('${link}', obj.absolute_url())
+                    body = body.replace('${status}', status.capitalize())
+                    body = body.replace('${requestor}', obj.title)
                     
-                body = """
-                    <div style="border: 1px solid #888; background-color: #eee; padding: 10px; margin-top: 10px; max-width: 500px;">
-                        <div style="font-weight: bold; margin-left: 10px;">Your leave request was <u>${status}</u> by ${approver}</div>
-                        <div style="margin: 10px;">${timeoff}</div>
-                        <div style="margin: 10px;">
-                            <a style="background-color:#2959af; color:white; display:inline-block; font-weight:bold; margin:10px; padding:5px 10px; text-decoration: none;" href="${link}">View Request</a>
-                        </div>
-                    </div> 
-                """
-                body = body.replace('${approver}', obj.supervisors)
-                body = body.replace('${timeoff}', TimeOffFormater(obj.timeoff))
-                body = body.replace('${link}', obj.absolute_url())
-                body = body.replace('${status}', status.capitalize())
-                body = body.replace('${requestor}', obj.title)
-                
-                MailMe('Leave Request - ' + status.capitalize(), obj.supervisors, [obj.email], body)
+                    MailMe('Leave Request - ' + status.capitalize(), obj.supervisors, [obj.email], body)
+                    
+                    
+                    # for office
+                    if obj.workflow_status == 'approved':
+                        AddEventMailed(token, obj.title, obj.supervisors, obj.timeoff)
+                    
                     
         if no_redirect == '0':        
             return self.request.response.redirect(obj.absolute_url())
